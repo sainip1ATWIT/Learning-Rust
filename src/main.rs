7a30991dc4cab1b7e024ad574e143e1e1e797445
sainip1 <sainip1@wit.edu>
2024-05-13 12:51:20 -0400

    Functions/Tuples/Arrays/For loops

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
     println!("Hello, world!");
-    
+
+    //assigning an integer
+    let x: i32 = 1;
+    //assigning a float
+    let y: f64 = 1.1;
+
+    println!("X is {}, Y is {}", x, y);
 }
